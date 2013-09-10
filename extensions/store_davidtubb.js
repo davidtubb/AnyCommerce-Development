eb2a8c81102525af59c75fe891dc7df5fb1536f9
Kim Horakh <kimh@zoovy.com>
2013-09-10 15:00:03 -0700

    Added Javascript for elastic search and featprod

--- extensions/store_davidtubb.js
+++ extensions/store_davidtubb.js
@@ -24,7 +24,7 @@ var store_davidtubb = function() {
 	var theseTemplates = new Array('');
 	var r = {
 
-
+	vars : {},
 ////////////////////////////////////   CALLBACKS    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
@@ -34,7 +34,37 @@ var store_davidtubb = function() {
 		init : {
 			onSuccess : function()	{
 				var r = false; //return false if extension won't load for some reason (account config, dependencies, etc).
-
+				
+				$.getJSON("_featuredprods.json", function(json){
+					app.ext.store_davidtubb.vars.featuredProdMap = json;
+				}).fail(function(){app.u.throwMessage("Featured Products failed to load due to an error in JSON encoding.")});
+				
+				app.rq.push(['templateFunction','categoryTemplate','onCompletes',function(P) {
+					var $context = $(app.u.jqSelector('#',P.parentID));
+					var featProd = app.ext.store_davidtubb.vars.featuredProdMap[P.navcat];
+					if(featProd){
+						
+						}
+					else {
+						//No featured product assigned to this category... default behavior?
+						featProd = "BNC";
+						}
+					var tagObj = {
+						'callback' : function(rd){
+							if(!app.model.responseHasErrors(rd)){
+								$('.featuredProductContainer', $context).anycontent({'datapointer':'appProductGet|'+featProd,'templateID':'featuredProductTemplate'});
+					
+								}
+							else {
+								app.u.throwMessage(rd);
+								}
+							}
+					}	
+					
+					app.ext.store_product.calls.appProductGet.init(featProd,tagObj,'immutable');
+					
+					}]);
+				
 				app.rq.push(['templateFunction','homepageTemplate','onCompletes',function(P) {
 					var $target=$('#wideSlideshow');
 					if(!$target.hasClass('slideshowSet')){ //target doesn't already have slideshow
@@ -61,9 +91,31 @@ var store_davidtubb = function() {
 					});	
 					}]);
 					
-					app.rq.push(['templateFunction','categoryTemplate','onCompletes',function(P) {
-						alert("This is an alert!");
-					}]);
+				app.rq.push(['templateFunction','categoryTemplate','onCompletes',function(P) {
+					var $context = $(app.u.jqSelector('#',P.parentID));
+					var elasticsearch = app.ext.store_search.u.buildElasticRaw({
+						"query" : {
+							"term" : {"app_category" : P.navcat }
+							},
+							"sort" : [
+								{ "base_price" : {"order" : "asc"}}
+							]
+						});
+					var _tag = {
+						'datapointer' : 'catSearchAscending|'+P.navcat,
+						'callback' : function(rd){
+							if(!app.model.responseHasErrors(rd)){
+								$('.priceList', $context).anycontent({'datapointer':rd.datapointer,'templateID':'priceListTemplate'})
+								}
+							else {
+								app.u.throwMessage(rd);
+								}
+							}
+						};
+					
+					app.ext.store_search.calls.appPublicProductSearch.init(elasticsearch, _tag, 'immutable');
+					app.model.dispatchThis('immutable');
+				}]);
 					
 				//if there is any functionality required for this extension to load, put it here. such as a check for async google, the FB object, etc. return false if dependencies are not present. don't check for other extensions.
 				r = true;
@@ -94,7 +146,9 @@ var store_davidtubb = function() {
 //on a data-bind, format: is equal to a renderformat. extension: tells the rendering engine where to look for the renderFormat.
 //that way, two render formats named the same (but in different extensions) don't overwrite each other.
 		renderFormats : {
-
+			test : function($tag, data){
+				app.u.dump(data.value);
+				}
 			}, //renderFormats
 ////////////////////////////////////   UTIL [u]   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
